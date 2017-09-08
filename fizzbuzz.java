1cf11460503436ca1778b1de24c3e79759a3948d
Kurt Daill <kdaill@luc.edu>
2017-09-08 14:45:51 +0000

    Part 2 Really Completed (Ealier Git Errors)

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,3 +1,33 @@
-public class fizzbuzz(){
+import java.util.Scanner;
 
+public class fizzbuzz{
+        public static void main(String[] args){
+        int n;
+        Scanner keyboard = new Scanner(System.in);
+        
+        while(true){
+        System.out.println("Please enter a positive integer, to set length.");
+        n = keyboard.nextInt();
+        if(n < 0){
+            System.out.println("Positive integers only.");
+            }else break;        
         }
+        
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0){
+                if(i % 5 == 0){
+                    System.out.println("Fizzbuzz");
+                    continue;
+                }else{
+                    System.out.println("Fizz");
+                    continue;
+                }
+            }
+            if(i % 5 == 0){
+                System.out.println("Buzz");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
